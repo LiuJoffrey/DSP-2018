abae3c96a96c1f7e2d31d0fb67e2d739a64dfeda
LiuJoffrey <r07921052@ntu.edu.tw>
2019-12-02 08:21:19 +0000

    change for argv

---
+++ dsp_hw3/run.sh
@@ -0,0 +1,7 @@
+./mydisambig test_data/4.txt ZhuYin-Big5.map bigram.lm result_my/4_out.txt
+./mydisambig test_data/5.txt ZhuYin-Big5.map bigram.lm result_my/5_out.txt
+./mydisambig test_data/6.txt ZhuYin-Big5.map bigram.lm result_my/6_out.txt
+./mydisambig test_data/7.txt ZhuYin-Big5.map bigram.lm result_my/7_out.txt
+./mydisambig test_data/8.txt ZhuYin-Big5.map bigram.lm result_my/8_out.txt
+./mydisambig test_data/9.txt ZhuYin-Big5.map bigram.lm result_my/9_out.txt
+./mydisambig test_data/10.txt ZhuYin-Big5.map bigram.lm result_my/10_out.txt
